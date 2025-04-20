43fa5fe660ce7324fabd6ce1b080fed8ddf558b1
Nanami-yk185 <elcookiemomja81@gmail.com>
2025-04-20 16:04:21 +0900

    ユーザー管理機能　修正2

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,20 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-# ニックネーム
-validates :nickname, presence: true
+  # ニックネーム
+  validates :nickname, presence: true
 
-# 名前（全角漢字・ひらがな・カタカナ）
-validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "全角で入力してください" }
-validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "全角で入力してください" }
+  # 名前（全角漢字・ひらがな・カタカナ）
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角で入力してください' }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角で入力してください' }
 
-# 名前カナ（全角カタカナ）
-validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "全角カタカナで入力してください" }
-validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "全角カタカナで入力してください" }
+  # 名前カナ（全角カタカナ）
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナで入力してください' }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナで入力してください' }
 
-# 生年月日
-validates :birth_date, presence: true
+  # 生年月日
+  validates :birth_date, presence: true
 
   # パスワード：半角英数字混合かつ6文字以上
-validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}\z/,message: "は半角英数字混合で6文字以上で入力してください"}
+  validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}\z/, message: '半角英数字混合で6文字以上で入力してください' }
 end
