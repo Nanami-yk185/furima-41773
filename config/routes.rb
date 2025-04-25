ee90a1f4f16d9874bb85c3c0e9571aee4efe55f1
Nanami-yk185 <elcookiemomja81@gmail.com>
2025-04-25 14:57:19 +0900

    商品詳細表示機能

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "items#index"
-  resources :posts
   resources :items
+  resources :items do
+    resources :purchases, only: [:index, :create]
+  end
 end
