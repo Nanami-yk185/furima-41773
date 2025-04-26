e56e5517255d47d52c50fb34ed8fea2ef8bd2375
Nanami-yk185 <elcookiemomja81@gmail.com>
2025-04-26 09:09:23 +0900

    商品情報編集機能2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update]
+  before_action :redirect_unless_owner, only: [:edit, :update]
   def index
     # @items = Item.all
     @items = Item.order('created_at DESC')
@@ -12,18 +14,16 @@ class ItemsController < ApplicationController
   def create
     @item = current_user.items.build(item_params)
     if @item.save
-      redirect_to root_path # ← 成功したらトップページへ @item
+      redirect_to root_path
     else
-      render :new, status: :unprocessable_entity # ← エラー時はそのままnewを再表示
+      render :new, status: :unprocessable_entity
     end
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
@@ -40,5 +40,13 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name, :description, :category_id, :status_id, :shipping_fee_id, :prefecture_id,
                                  :shipping_day_id, :image, :price)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def redirect_unless_owner
+    redirect_to root_path unless current_user == @item.user
   end
 end
