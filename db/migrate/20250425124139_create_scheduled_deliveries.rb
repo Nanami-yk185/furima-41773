b1102f82c0bdb0d132bae44e6b99fdb8cf668c8d
Nanami-yk185 <elcookiemomja81@gmail.com>
2025-04-26 14:41:39 +0900

    商品情報編集機能　修正2

--- db/migrate/20250425124139_create_scheduled_deliveries.rb
+++
@@ -1,9 +0,0 @@
-class CreateScheduledDeliveries < ActiveRecord::Migration[7.1]
-  def change
-    create_table :scheduled_deliveries do |t|
-      t.string :name
-
-      t.timestamps
-    end
-  end
-end
